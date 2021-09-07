22f9f9bc32d107070b6db22424ace0c418a23078
Sergei Evdokimov <se.evdokimov@gmail.com>
2021-09-07 10:53:05 +0200

    cleaned mtsd loading code

--- file_list.py
+++ file_list.py
@@ -4,6 +4,7 @@
 from PIL import Image
 import torch.utils.data as data
 import os
+import json
 
 
 def default_loader(path):
@@ -24,8 +25,13 @@ def default_flist_reader(flist):
 
 
 class ImageFilelist(data.Dataset):
-    def __init__(self, root, flist, transform=None, target_transform=None,
-                 flist_reader=default_flist_reader, loader=default_loader):
+    def __init__(self,
+                 root,
+                 flist,
+                 transform=None,
+                 target_transform=None,
+                 flist_reader=default_flist_reader,
+                 loader=default_loader):
         self.root = root
         self.imlist = flist_reader(flist)
         self.transform = transform
@@ -47,15 +53,44 @@ class ImageFilelist(data.Dataset):
         return len(self.imlist)
 
 
-class ImageAnnotationfiles(data.Dataset):
-    def __init__(self, root, flist, annotations_dir, images_dir, transform=None, target_transform=None,
-                 annotations_reader=None, loader=default_loader):
+class MTSDDataset(data.Dataset):
+    def __init__(self,
+                 root,
+                 flist,
+                 annotations_dir,
+                 images_dir,
+                 transform=None,
+                 target_transform=None,
+                 loader=default_loader):
         self.root = root
-        self.imlist = annotations_reader(root, flist, annotations_dir, images_dir)
+        self.imlist = self.annotations_reader(root, flist, annotations_dir, images_dir)
         self.transform = transform
         self.target_transform = target_transform
         self.loader = loader
         self.targets = [x[1] for x in self.imlist]
+
+    def mtsd_annotations_reader(self, root, flist, annotations_dir, images_dir):
+        imlist = []
+        with open(os.path.join(root, flist), 'r') as rf:
+            for line in rf.readlines():
+                impath = os.path.join(root, images_dir, line.rstrip() + '.jpg')
+                anpath = os.path.join(root, annotations_dir, line.rstrip() + '.json')
+                try:
+                    if os.path.isfile(impath):
+                        with open(anpath, 'r') as afile:
+                            annotation = json.load(afile)
+                            labels = [item.get('label', None) for item in annotation.get('objects', {})]
+                            is_speed_limit = 0
+                            for label in labels:
+                                if label.startswith('regulatory--maximum-speed-limit'):
+                                    is_speed_limit = 1
+                                    break
+                            imlist.append((os.path.join(images_dir, line.rstrip() + '.jpg'), is_speed_limit))
+                    else:
+                        open(impath, 'r')
+                except FileNotFoundError as e:
+                    print("File not found: " + e.filename)
+        return imlist
 
     def __getitem__(self, index):
         impath, target = self.imlist[index]
@@ -69,4 +104,3 @@ class ImageAnnotationfiles(data.Dataset):
 
     def __len__(self):
         return len(self.imlist)
-
--- mtsd_speed_limit.py
+++ mtsd_speed_limit.py
@@ -25,7 +25,7 @@ import copy
 import os
 import json
 
-from file_list import ImageAnnotationfiles
+from file_list import MTSDDataset
 from data.mtsd_v2_fully_annotated import visualize_example
 
 plt.ion()  # interactive mode
@@ -35,65 +35,25 @@ plt.ion()  # interactive mode
 Data loader for original Mapillary MTSD dataset with two classes: 1 for an image with a speed limit and 0 without
 """
 
-
-def mtsd_annotations_reader(root, flist, annotations_dir, images_dir):
-    imlist = []
-    with open(os.path.join(root, flist), 'r') as rf:
-        for line in rf.readlines():
-            impath = os.path.join(root, images_dir, line.rstrip() + '.jpg')
-            anpath = os.path.join(root, annotations_dir,
-                                  line.rstrip() + '.json')
-            try:
-                if os.path.isfile(impath):
-                    with open(anpath, 'r') as afile:
-                        annotation = json.load(afile)
-                        labels = [
-                            item.get('label', None)
-                            for item in annotation.get('objects', {})
-                        ]
-                        is_speed_limit = 0
-                        for label in labels:
-                            if label.startswith(
-                                    'regulatory--maximum-speed-limit'):
-                                is_speed_limit = 1
-                                break
-                        imlist.append((os.path.join(images_dir,
-                                                    line.rstrip() + '.jpg'),
-                                       is_speed_limit))
-                else:
-                    open(impath, 'r')
-            except FileNotFoundError as e:
-                print("File not found: " + e.filename)
-
-    return imlist
-
-
-data_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+data_transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
 image_datasets = {
     'train':
-    ImageAnnotationfiles(root='./data/mtsd_v2_fully_annotated/',
-                         flist='splits/train.txt',
-                         annotations_dir='annotations/',
-                         images_dir='images/',
-                         transform=data_transform,
-                         annotations_reader=mtsd_annotations_reader),
+    MTSDDataset(root='./data/mtsd_v2_fully_annotated/',
+                flist='splits/train.txt',
+                annotations_dir='annotations/',
+                images_dir='images/',
+                transform=data_transform),
     'val':
-    ImageAnnotationfiles(root='./data/mtsd_v2_fully_annotated/',
-                         flist='splits/val.txt',
-                         annotations_dir='annotations/',
-                         images_dir='images/',
-                         transform=data_transform,
-                         annotations_reader=mtsd_annotations_reader)
+    MTSDDataset(root='./data/mtsd_v2_fully_annotated/',
+                flist='splits/val.txt',
+                annotations_dir='annotations/',
+                images_dir='images/',
+                transform=data_transform)
 }
 dataloaders = {
-    x: torch.utils.data.DataLoader(image_datasets[x],
-                                   batch_size=4,
-                                   shuffle=True,
-                                   num_workers=4)
+    x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4)
     for x in ['train', 'val']
 }
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
